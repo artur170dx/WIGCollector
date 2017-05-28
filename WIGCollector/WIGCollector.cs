fd3ee79c3f349a598d4c64c2deaa702c837db87a
Artur <czartur@gmail.com>
2017-05-28 14:32:11 +0200

    Delete redundant interval

--- WIGCollector/WIGCollector.cs
+++ WIGCollector/WIGCollector.cs
@@ -8,16 +8,20 @@ using WIGCollector.Model;
 
 namespace WIGCollector
 {
+    /* Aplikacja pobiera kursy ze strony http://stooq.pl <http://stooq.pl/> . 
+     * Dokładniej aplikacja ma pobierać w określonych konfigurowalnych 
+     * odstępach czasowych (wstępnie co 1 minutę) wartości WIG, WIG20, WIG20 
+     * Fut, mWIG40, sWIG80 i zapisywać w wybranym miejscu 
+     * czas pobrania i wartości. Wartości zapisujemy tylko wtedy
+     * gdy się zmieniły od poprzedniego razu.*/
     public class WIGCollector
     {
         private TimeSpan defaultInterval = new TimeSpan(0, 1, 0);
         private Timer timer;
-        private TimeSpan interval;
         private CollectorBrain brain;
       
         public WIGCollector()
         {
-            interval = defaultInterval;
             brain = new CollectorBrain(Factory.GetDownloader(), Factory.GetDataBase());
             TimerCallback tmCallback = Callback;
             timer = new Timer(tmCallback, "", Timeout.Infinite, Timeout.Infinite);
@@ -28,14 +32,10 @@ namespace WIGCollector
             TimerOn();
         }
 
-        public void ChangeInterval(int intervalInSeconds)
+        public void ChangeInterval(uint intervalInSeconds)
         {
-            TimeSpan requestedInterval = new TimeSpan(0, 0, intervalInSeconds);
+            TimeSpan requestedInterval = new TimeSpan(0, 0, Convert.ToInt32(intervalInSeconds));
             bool timerSuccessfullyChanged = timer.Change(requestedInterval, requestedInterval);
-            if (timerSuccessfullyChanged)
-            {
-                interval = requestedInterval;
-            }
         }
 
         private void Callback(object objectInfo)
@@ -45,7 +45,7 @@ namespace WIGCollector
 
         private void TimerOn()
         {
-            timer.Change(interval, interval);
+            timer.Change(defaultInterval, defaultInterval);
         }
     }
 }
--- WIGCollectorView/Program.cs
+++ WIGCollectorView/Program.cs
@@ -14,7 +14,11 @@ namespace WIGCollectorView
             WIGCollector.WIGCollector brain = new WIGCollector.WIGCollector();
             brain.ChangeInterval(10);
             brain.Run();
-            Console.Read();
+            ConsoleKeyInfo key = new ConsoleKeyInfo();
+            while (key.Key != ConsoleKey.Escape)
+            {
+                Console.Read();
+            }
         }
     }
 }
